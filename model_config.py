9c4ebb3393332fa69a3ecc48eb6b05257e044304
BahSali <145577450+BahSali@users.noreply.github.com>
2024-09-02 21:33:41 +0330

    Update model_config.py

--- model_config.py
+++ model_config.py
@@ -2,10 +2,9 @@ from data_prepration import scaled_laplacian_matrix, adjacency_matrix, cheb_poly
 import torch
 
 
-def set_backbones(adj_filename, num_of_vertices, num_of_weeks, num_of_days, num_of_hours, k):
-    adj_mx = adjacency_matrix(adj_filename, num_of_vertices)  # shape: num_of_vertices * num_of_vertices
+def set_backbones(adj_filename, num_of_vertices, num_of_weeks, num_of_days, num_of_hours, k, roads_names):
+    adj_mx = adjacency_matrix(adj_filename, num_of_vertices, roads_names)  # shape: num_of_vertices * num_of_vertices
     lp_mx = scaled_laplacian_matrix(adj_mx)  # shape: num_of_vertices * num_of_vertices
-
     cheb_polynomials = [torch.tensor(i, device='cpu') for i in cheb_polynomial(lp_mx, k)]
     # shape: k * num_of_vertices * num_of_vertices
 
@@ -14,6 +13,7 @@ def set_backbones(adj_filename, num_of_vertices, num_of_weeks, num_of_days, num_
             "K": k,
             "num_of_chev_filters": 64,
             "num_of_time_filters": 64,
+            "num_of_input_channels": 1,
             "time_conv_strides": num_of_weeks,
             "cheb_polynomials": cheb_polynomials
         },
@@ -21,6 +21,7 @@ def set_backbones(adj_filename, num_of_vertices, num_of_weeks, num_of_days, num_
             "K": k,
             "num_of_chev_filters": 64,
             "num_of_time_filters": 64,
+            "num_of_input_channels": 64,
             "time_conv_strides": 1,
             "cheb_polynomials": cheb_polynomials
         }
@@ -31,6 +32,7 @@ def set_backbones(adj_filename, num_of_vertices, num_of_weeks, num_of_days, num_
             "K": k,
             "num_of_chev_filters": 64,
             "num_of_time_filters": 64,
+            "num_of_input_channels": 1,
             "time_conv_strides": num_of_days,
             "cheb_polynomials": cheb_polynomials
         },
@@ -38,6 +40,7 @@ def set_backbones(adj_filename, num_of_vertices, num_of_weeks, num_of_days, num_
             "K": k,
             "num_of_chev_filters": 64,
             "num_of_time_filters": 64,
+            "num_of_input_channels": 64,
             "time_conv_strides": 1,
             "cheb_polynomials": cheb_polynomials
         }
@@ -48,6 +51,7 @@ def set_backbones(adj_filename, num_of_vertices, num_of_weeks, num_of_days, num_
             "K": k,
             "num_of_chev_filters": 64,
             "num_of_time_filters": 64,
+            "num_of_input_channels": 1,
             "time_conv_strides": num_of_hours,
             "cheb_polynomials": cheb_polynomials
         },
@@ -55,6 +59,7 @@ def set_backbones(adj_filename, num_of_vertices, num_of_weeks, num_of_days, num_
             "K": k,
             "num_of_chev_filters": 64,
             "num_of_time_filters": 64,
+            "num_of_input_channels": 64,
             "time_conv_strides": 1,
             "cheb_polynomials": cheb_polynomials
         }
